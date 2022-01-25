ea5f40f68f407259c7821ffe904e3ed98622e09a
Hai Minh <haiminh.d@samsung.com>
2022-01-25 08:19:46 +0700

    Init Project

---
+++ app/src/main/java/com/miin/learning/memorynote/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.miin.learning.memorynote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
