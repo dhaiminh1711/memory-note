22687e36573bc0a4362c292bba36318e20061f43
Hai Minh <haiminh.d@samsung.com>
2022-01-25 09:28:26 +0700

    Create note repository

---
+++ core/src/main/java/com/miin/learning/core/repository/NoteDataSource.kt
@@ -0,0 +1,13 @@
+package com.miin.learning.core.repository
+
+import com.miin.learning.core.data.Note
+
+interface NoteDataSource {
+    suspend fun add(note: Note)
+
+    suspend fun get(id: Long): Note?
+
+    suspend fun getAll(): List<Note>
+
+    suspend fun remove(note: Note)
+}
