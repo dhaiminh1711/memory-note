34af94729eb6e3632b6a055761491851024efc0a
Hai Minh <haiminh.d@samsung.com>
2022-01-25 14:10:11 +0700

    Create room database for note

---
+++ app/src/main/java/com/miin/learning/memorynote/framework/UseCases.kt
@@ -0,0 +1,13 @@
+package com.miin.learning.memorynote.framework
+
+import com.miin.learning.core.usecase.AddNote
+import com.miin.learning.core.usecase.GetAllNotes
+import com.miin.learning.core.usecase.GetNote
+import com.miin.learning.core.usecase.RemoveNote
+
+data class UseCases(
+    val addNote: AddNote,
+    val getAllNotes: GetAllNotes,
+    val getNote: GetNote,
+    val removeNote: RemoveNote,
+)
