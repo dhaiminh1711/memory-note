bbdad641ac72e72c70f92199a1a8d44c0cb83944
Hai Minh <haiminh.d@samsung.com>
2022-01-25 08:40:50 +0700

    Setup folder

--- app/src/main/java/com/miin/learning/memorynote/presentation/MainActivity.kt
+++ app/src/main/java/com/miin/learning/memorynote/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.miin.learning.memorynote
+package com.miin.learning.memorynote.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.miin.learning.memorynote.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
