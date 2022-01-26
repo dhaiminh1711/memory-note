9d8c34ab4f378c82ea391793302cc1787cf8d9c3
Hai Minh <haiminh.d@samsung.com>
2022-01-26 15:21:54 +0700

    Apply MVVM for note fragment

---
+++ app/src/main/java/com/miin/learning/memorynote/framework/NoteViewModel.kt
@@ -0,0 +1,39 @@
+package com.miin.learning.memorynote.framework
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.miin.learning.core.data.Note
+import com.miin.learning.core.repository.NoteRepository
+import com.miin.learning.core.usecase.AddNote
+import com.miin.learning.core.usecase.GetAllNotes
+import com.miin.learning.core.usecase.GetNote
+import com.miin.learning.core.usecase.RemoveNote
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    val repository = NoteRepository(RoomNoteDataSource(application))
+
+    val useCases = UseCases(
+        AddNote(repository),
+        GetAllNotes(repository),
+        GetNote(repository),
+        RemoveNote(repository),
+    )
+
+    private val _saved = MutableLiveData<Boolean>()
+    val saved: LiveData<Boolean>
+        get() = _saved
+
+    fun savedNote(note: Note) {
+        coroutineScope.launch {
+            useCases.addNote(note)
+            _saved.postValue(true)
+        }
+    }
+}
--- app/src/main/java/com/miin/learning/memorynote/presentation/NoteFragment.kt
+++ app/src/main/java/com/miin/learning/memorynote/presentation/NoteFragment.kt
@@ -1,15 +1,24 @@
 package com.miin.learning.memorynote.presentation
 
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
+import com.miin.learning.core.data.Note
 import com.miin.learning.memorynote.databinding.FragmentNoteBinding
+import com.miin.learning.memorynote.framework.NoteViewModel
 
 class NoteFragment : Fragment() {
     private lateinit var binding: FragmentNoteBinding
+    private lateinit var viewModel: NoteViewModel
+    private var currentNote = Note("", "", 0L, 0L)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +36,47 @@ class NoteFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProviders.of(this).get(NoteViewModel::class.java)
+
         binding.apply {
-            saveBtn.setOnClickListener { Navigation.findNavController(it).popBackStack() }
+            saveBtn.setOnClickListener {
+                if (titleView.text.toString() != "" || contentView.text.toString() != "") {
+                    val time = System.currentTimeMillis()
+                    currentNote.apply {
+                        title = titleView.text.toString()
+                        content = contentView.text.toString()
+                        updateTime = time
+                        if (id == 0L) {
+                            creationTime = time
+                        }
+                        viewModel.savedNote(currentNote)
+                    }
+                } else {
+                    Navigation.findNavController(it).popBackStack()
+                }
+            }
         }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.saved.observe(viewLifecycleOwner) { isSaved ->
+            if (isSaved) {
+                Toast.makeText(context, "Done", Toast.LENGTH_SHORT).show()
+                hideKeyboard()
+                Navigation.findNavController(binding.titleView).popBackStack()
+            } else {
+                Toast.makeText(context,
+                    "Something went wrong, please try again",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun hideKeyboard() {
+        val imm = context?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.titleView.windowToken, 0)
     }
 
     companion object {
